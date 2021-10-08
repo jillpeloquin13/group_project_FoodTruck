1acfa243d652e0dc5e20cd0a240120312213cf65
Jill Peloquin <jill.peloquin13@gmail.com>
2021-10-07 20:55:42 -0400

    working on data

---
+++ FoodTruckETLv2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import requests
+
+
+def getdata():
+
+    url = 'http://data.streetfoodapp.com/1.1/schedule/vancouver'
+    data =  requests.get(url).json()
+    dfVan = pd.DataFrame.from_dict(data)
+    dfVan.head()
+
+    url = 'http://data.streetfoodapp.com/1.1/schedule/boston'
+    data=  requests.get(url).json()
+    dfBos = pd.DataFrame.from_dict(data)
+    dfBos.head()
+
+    url = 'http://data.streetfoodapp.com/1.1/schedule/tallahassee'
+    data=  requests.get(url).json()
+    dfTalla = pd.DataFrame.from_dict(data)
+    dfTalla.head()
+
+
+
+
+    return dfVan, dfBos, dfTalla
